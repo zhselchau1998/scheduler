5e743f33c5f1e983b3be2c97dbcbb49e2a055fb6
Zach Selchau <zhselchau1998@yahoo.com>
2019-02-07 15:26:49 -0800

    fixed the arg checker for '-b'

--- xbd.cpp
+++ xbd.cpp
@@ -8,20 +8,21 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     
-    bool isOutBinary = false;//Hex format currently
-    bool isFileBinary = false;//Ascii format currently
-    int fileArgIndex = 2;//Binary format currently
+    bool isOutBinary = false;       //Hex format currently
+    bool isFileBinary = false;      //Ascii format currently
+    int fileArgIndex = 2;           //Binary format currently
 
     //First check for args
-    if(argc == 3)
-        if(strstr(argv[1], "-b") == 0)
-            isOutBinary = true;    //Change format to binary
-        else
-            fileArgIndex = 1;   //Change format to hex
+    if(strcmp(argv[1], "-b") == 0)
+        isOutBinary = true;         //Change format to binary
+    else
+        fileArgIndex = 1;           //Change format to hex
     
     //Second create ifstream 
     string fileName(argv[fileArgIndex]);
     ifstream infile(fileName);
+
+    cout << fileName << "\n";
 
     //Third put ifstream into sstream
     if(infile){
