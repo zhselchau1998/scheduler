a12a3f1b34c24cff0231bfc94e9eeb741fd0cb09
Darpan Beri <darpanberi.99@gmail.com>
2019-02-09 23:18:35 +0000

    Made Comments neater

--- xbd.cpp
+++ xbd.cpp
@@ -10,6 +10,12 @@
 
 using namespace std;
 
+/* OUT INDEX FORMATTING FUNCTION
+        1. For the counter, intialized index as decimal named 'decIndex'. Put in decimal to hex. Make copy of return(which is a string now) called 'hexIndex'.
+        2. While(hexIndex.length() < 8):
+        3. Add 00s to the front of the string.
+*/
+
 int binaryToDecimal(string binaryString){
 
     double sum = 0;
@@ -237,31 +243,33 @@ int main(int argc, char* argv[]){
             }
         }
 
-        //~ isFileBinary = True:
-          // Make a copy of fullText called binaryText.
-          // While loop for taking 8 char chunk after checking if the string has 8 chars avail.
-          // 1 Put 8 char chunk in 'binary_chunk' and make a copy of 'binary_chunk' named 'binary_string'.
-          // 1.b If isOutBinary = True, put 8 char chunk in 'binary_chunk'.
-          // 2. Convert that 'binary_chunk' into 'hex_string' and 'binary_string'into 'Ascii_char'. If not convertable to Ascii, put the character '.'.
-          // 2.b Convert 'binary chunk' to 'Ascii_char'. If not convertable to Ascii, put the character '.'.
-          // 3. Put 'hex_string' in hexText and 'Ascii_char' in asciiText.
-          // 3.b We already have a copy called binaryText. Put 'Ascii_char' in asciiText.
+        /*~ isFileBinary = True:
+          Make a copy of fullText called binaryText.
+          While loop for taking 8 char chunk after checking if the string has 8 chars avail.
+          1 Put 8 char chunk in 'binary_chunk' and make a copy of 'binary_chunk' named 'binary_string'.
+          1.b If isOutBinary = True, put 8 char chunk in 'binary_chunk'.
+          2. Convert that 'binary_chunk' into 'hex_string' and 'binary_string'into 'Ascii_char'. If not convertable to Ascii, put the character '.'.
+          2.b Convert 'binary chunk' to 'Ascii_char'. If not convertable to Ascii, put the character '.'.
+          3. Put 'hex_string' in hexText and 'Ascii_char' in asciiText.
+          3.b We already have a copy called binaryText. Put 'Ascii_char' in asciiText.*/
 
-        //~ isFileBinary = False:
-          // Make a copy of fullText called asciiText.
-          // While loop taking 2 char chunk after checking if the string has 2 chars avail.
-          //1. Put 2 char chunk in 'ascii_chunk'
-          //1.b If isOutBinary = True, Put 2 char chunk in 'ascii_chunk'.
-          //2. Convert 'ascii_chunk' to 'hex_string'.
-          //2.b Convert 'ascii_chunk' to 'hex_string' and then 'hex_string' to 'binary_string'.
-          //3. Put 'hex_string' to hexText. We already have asciiText.
-          //3.b Put 'binary_string' to binaryText. We already have asciiText.
+        /* ~ isFileBinary = False:
+          Make a copy of fullText called asciiText.
+          While loop taking 2 char chunk after checking if the string has 2 chars avail.
+          1. Put 2 char chunk in 'ascii_chunk'
+          1.b If isOutBinary = True, Put 2 char chunk in 'ascii_chunk'.
+          2. Convert 'ascii_chunk' to 'hex_string'.
+          2.b Convert 'ascii_chunk' to 'hex_string' and then 'hex_string' to 'binary_string'.
+          3. Put 'hex_string' to hexText. We already have asciiText.
+          3.b Put 'binary_string' to binaryText. We already have asciiText. */
 
         //Sixth output result
 
-        //OUTPUT FORMAT: I = Index column, h = hex, b = binary and a = ascii
-        // IIIIIII hhhh hhhh hhhh hhhh hhhh hhhh hhhh hhhh aaaaaaaaaaaaaaaa
-        // IIIIIII bbbbbbbb bbbbbbbb bbbbbbbb bbbbbbbb bbbbbbbb bbbbbbbb aaaaaa
+        /* OUTPUT FORMAT: I = Index column, h = hex, b = binary and a = ascii
+        if isOutBinary = False;
+        IIIIIII hhhh hhhh hhhh hhhh hhhh hhhh hhhh hhhh aaaaaaaaaaaaaaaa
+        if isOutBinary = True
+        IIIIIII bbbbbbbb bbbbbbbb bbbbbbbb bbbbbbbb bbbbbbbb bbbbbbbb aaaaaa */
 
         //cout << asciiText << endl;         //Testing SS
             
