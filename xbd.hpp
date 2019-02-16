d7d7cafba4ceb049980bfaf9fc589b99001da971
Darpan Beri <darpanberi.99@gmail.com>
2019-02-16 21:23:13 +0000

    Completed header file.

--- xbd.hpp
+++ xbd.hpp
@@ -7,3 +7,25 @@
 #include <algorithm>
 #include <cstdlib>
 #include <cmath>
+
+using namespace std;
+
+int binaryToDecimal(string binaryString);
+
+string binaryToAscii(string bin);
+
+string decimalToHex(int num);
+
+string decimalToBinary(int num);
+
+string asciiToBinary(string str);
+
+string binaryToHex(string binaryString);
+
+string asciiToHex(string asciiString);
+
+string decimalToAddress(int num);
+
+string addSpacesToBinary(string binary);
+
+string addSpacesToHex(string hex);
